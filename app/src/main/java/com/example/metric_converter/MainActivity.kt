19fd8aac07d0418d90650a7e270e32dc53ec03bd
ebrone <hebronprasetya26@gmail.com>
2024-11-03 22:34:49 +0800

    upload2

--- app/src/main/java/com/example/metric_converter/MainActivity.kt
+++ app/src/main/java/com/example/metric_converter/MainActivity.kt
@@ -3,53 +3,48 @@ package com.example.metric_converter
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
 
     // Deklarasi variabel XML ke dalam kode Kotlin
     private lateinit var spMetrics: Spinner
-    private lateinit var spOriginal: Spinner
-    private lateinit var spConvert: Spinner
+    private lateinit var spOriginalUnit: Spinner
+    private lateinit var spTargetUnit: Spinner
     private lateinit var inputValue: EditText
     private lateinit var resultText: TextView
 
     // Fungsi untuk menghitung hasil konversi
-    private fun calculateResult(input: Double, index: Int, index2: Int, index3: Int) {
+    private fun calculateResult(input: Double, metricIndex: Int, originalIndex: Int, targetIndex: Int) {
         var result = input
-        var index4: Int
+        var indexDifference: Int
 
-        if (index in 1..4) { // Untuk panjang, massa, waktu, arus listrik
-            index4 = index2 - index3
-            if (index4 > 0) {
-                for (x in 1..index4)
+        if (metricIndex in 1..4 || metricIndex in 6..7) { // Untuk panjang, massa, waktu, arus listrik, intensitas cahaya, jumlah zat
+            indexDifference = originalIndex - targetIndex
+            if (indexDifference > 0) {
+                for (x in 1..indexDifference)
                     result *= 10
-            } else if (index4 < 0) {
-                index4 = abs(index4)
-                for (x in 1..index4)
+            } else if (indexDifference < 0) {
+                indexDifference = abs(indexDifference)
+                for (x in 1..indexDifference)
                     result /= 10
             }
             resultText.text = "$result"
-        } else if (index == 5) { // Untuk suhu
+        } else if (metricIndex == 5) { // Untuk suhu
             result = when {
-                index2 == 0 && index3 == 1 -> (input * 9 / 5) + 32 // Celcius -> Fahrenheit
-                index2 == 0 && index3 == 2 -> input + 273.15 // Celcius -> Kelvin
-                index2 == 1 && index3 == 0 -> (input - 32) * 5 / 9 // Fahrenheit -> Celcius
-                index2 == 1 && index3 == 2 -> ((input - 32) * 5 / 9) + 273.15 // Fahrenheit -> Kelvin
-                index2 == 2 && index3 == 0 -> input - 273.15 // Kelvin -> Celcius
-                index2 == 2 && index3 == 1 -> ((input - 273.15) * 9 / 5) + 32 // Kelvin -> Fahrenheit
+                originalIndex == 0 && targetIndex == 1 -> (input * 9 / 5) + 32 // Celcius -> Fahrenheit
+                originalIndex == 0 && targetIndex == 2 -> input + 273.15 // Celcius -> Kelvin
+                originalIndex == 1 && targetIndex == 0 -> (input - 32) * 5 / 9 // Fahrenheit -> Celcius
+                originalIndex == 1 && targetIndex == 2 -> ((input - 32) * 5 / 9) + 273.15 // Fahrenheit -> Kelvin
+                originalIndex == 2 && targetIndex == 0 -> input - 273.15 // Kelvin -> Celcius
+                originalIndex == 2 && targetIndex == 1 -> ((input - 273.15) * 9 / 5) + 32 // Kelvin -> Fahrenheit
                 else -> input
             }
             resultText.text = "$result"
@@ -60,69 +55,80 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         // Menghubungkan komponen XML dengan variabel Kotlin
         inputValue = findViewById(R.id.getInputValue)
         resultText = findViewById(R.id.resultText)
 
         // Daftar satuan untuk setiap metrik
-        val lengths = listOf("Milimeter", "Centimeter", "Desimeter", "Meter", "Dekameter", "Hektometer", "Kilometer")
-        val mass = listOf("Miligram", "Centigram", "Desigram", "Gram", "Dekagram", "Hektogram", "Kilogram")
-        val time = listOf("Milisekon", "Centisekon", "Desisekon", "Sekon", "Dekasekon", "Hektosekon", "Kilosekon")
-        val electricCurrent = listOf("Miliampere", "Centiampere", "Desiampere", "Ampere", "Dekaampere", "Hektoampere", "Kiloampere")
-        val temperature = listOf("Celcius", "Fahrenheit", "Kelvin")
+        val lengthUnits = listOf("Milimeter", "Centimeter", "Desimeter", "Meter", "Dekameter", "Hektometer", "Kilometer")
+        val massUnits = listOf("Miligram", "Centigram", "Desigram", "Gram", "Dekagram", "Hektogram", "Kilogram")
+        val timeUnits = listOf("Milisekon", "Centisekon", "Desisekon", "Sekon", "Dekasekon", "Hektosekon", "Kilosekon")
+        val electricCurrentUnits = listOf("Miliampere", "Centiampere", "Desiampere", "Ampere", "Dekaampere", "Hektoampere", "Kiloampere")
+        val temperatureUnits = listOf("Celcius", "Fahrenheit", "Kelvin")
+        val intensitasCahaya = listOf("Milicandela", "Candela", "Kilocandela") // Satuan intensitas cahaya
+        val jumlahZat = listOf("Millimol", "Mol", "Kilomol") // Satuan jumlah zat
 
         // Mengatur adapter untuk spinner satuan
-        val adapterLengths = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, lengths)
-        val adapterMass = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, mass)
-        val adapterTime = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, time)
-        val adapterElectricCurrent = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, electricCurrent)
-        val adapterTemperature = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, temperature)
+        val adapterLength = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, lengthUnits)
+        val adapterMass = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, massUnits)
+        val adapterTime = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, timeUnits)
+        val adapterElectricCurrent = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, electricCurrentUnits)
+        val adapterTemperature = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, temperatureUnits)
+        val adapterIntensitasCahaya = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, intensitasCahaya)
+        val adapterJumlahZat = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, jumlahZat)
 
-        var indexMetric = 0
-        var indexMetric2 = 0
-        var indexMetric3 = 0
+        var metricIndex = 0
+        var originalIndex = 0
+        var targetIndex = 0
 
-        var getInputNum = 0.0
+        var inputNumber = 0.0
 
         // Menghubungkan spinner ke variabel Kotlin
-        spOriginal = findViewById(R.id.spOriginal)
-        spOriginal.isEnabled = false
+        spOriginalUnit = findViewById(R.id.spOriginal)
+        spOriginalUnit.isEnabled = false
 
-        spConvert = findViewById(R.id.spConvert)
-        spConvert.isEnabled = false
+        spTargetUnit = findViewById(R.id.spConvert)
+        spTargetUnit.isEnabled = false
 
         spMetrics = findViewById(R.id.spMetrics)
 
         // Mengatur pilihan metrik
         spMetrics.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                spOriginal.isEnabled = position != 0
-                spConvert.isEnabled = position != 0
+                spOriginalUnit.isEnabled = position != 0
+                spTargetUnit.isEnabled = position != 0
 
                 when (position) {
                     1 -> { // Panjang
-                        spOriginal.adapter = adapterLengths
-                        spConvert.adapter = adapterLengths
+                        spOriginalUnit.adapter = adapterLength
+                        spTargetUnit.adapter = adapterLength
                     }
                     2 -> { // Massa
-                        spOriginal.adapter = adapterMass
-                        spConvert.adapter = adapterMass
+                        spOriginalUnit.adapter = adapterMass
+                        spTargetUnit.adapter = adapterMass
                     }
                     3 -> { // Waktu
-                        spOriginal.adapter = adapterTime
-                        spConvert.adapter = adapterTime
+                        spOriginalUnit.adapter = adapterTime
+                        spTargetUnit.adapter = adapterTime
                     }
                     4 -> { // Arus listrik
-                        spOriginal.adapter = adapterElectricCurrent
-                        spConvert.adapter = adapterElectricCurrent
+                        spOriginalUnit.adapter = adapterElectricCurrent
+                        spTargetUnit.adapter = adapterElectricCurrent
                     }
                     5 -> { // Suhu
-                        spOriginal.adapter = adapterTemperature
-                        spConvert.adapter = adapterTemperature
+                        spOriginalUnit.adapter = adapterTemperature
+                        spTargetUnit.adapter = adapterTemperature
+                    }
+                    6 -> { // Intensitas Cahaya
+                        spOriginalUnit.adapter = adapterIntensitasCahaya
+                        spTargetUnit.adapter = adapterIntensitasCahaya
+                    }
+                    7 -> { // Jumlah Zat
+                        spOriginalUnit.adapter = adapterJumlahZat
+                        spTargetUnit.adapter = adapterJumlahZat
                     }
                 }
-                indexMetric = position
+                metricIndex = position
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -131,11 +137,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Ketika satuan asal dipilih
-        spOriginal.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        spOriginalUnit.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                indexMetric2 = position
-                if (getInputNum != 0.0 && indexMetric3 != 0) {
-                    calculateResult(getInputNum, indexMetric, indexMetric2, indexMetric3)
+                originalIndex = position
+                if (inputNumber != 0.0 && targetIndex != 0) {
+                    calculateResult(inputNumber, metricIndex, originalIndex, targetIndex)
                 }
             }
 
@@ -145,11 +151,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Ketika satuan tujuan dipilih
-        spConvert.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        spTargetUnit.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                indexMetric3 = position
-                if (getInputNum != 0.0 && indexMetric2 != 0) {
-                    calculateResult(getInputNum, indexMetric, indexMetric2, indexMetric3)
+                targetIndex = position
+                if (inputNumber != 0.0 && originalIndex != 0) {
+                    calculateResult(inputNumber, metricIndex, originalIndex, targetIndex)
                 }
             }
 
@@ -161,10 +167,10 @@ class MainActivity : AppCompatActivity() {
         // Mendengarkan perubahan teks pada input nilai
         inputValue.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                val getString: String = s?.toString() ?: ""
-                getInputNum = if (getString.isNotEmpty()) {
+                val inputString: String = s?.toString() ?: ""
+                inputNumber = if (inputString.isNotEmpty()) {
                     try {
-                        getString.toDouble()
+                        inputString.toDouble()
                     } catch (e: NumberFormatException) {
                         0.0
                     }
@@ -172,8 +178,8 @@ class MainActivity : AppCompatActivity() {
                     0.0
                 }
 
-                if (getInputNum != 0.0) {
-                    calculateResult(getInputNum, indexMetric, indexMetric2, indexMetric3)
+                if (inputNumber != 0.0) {
+                    calculateResult(inputNumber, metricIndex, originalIndex, targetIndex)
                 }
             }
 
